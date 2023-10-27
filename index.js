1399a16c33b5a6fa757865b800810824064fc637
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-10-27 21:55:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverse = word.split('').reverse().join('');
+  if (word === reverse)  {
+    return true;
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  the inut should be a string
+  the function should take the string and rearrange the charaters from back to front
+  the function should check if the resulting string is a alidrome of the original input
+  if it is should output boolean true
+  if it isnt output should be boolean false 
 */
 
 /*
   Add written explanation of your solution here
+  the function parses a string as the arguement and thus takes the string and splits the
+  charaters into individual letter and rearranges them in a reverse order thus creating a different array.
+  it then rejoins the array to form a string and checks the sting to see if it is similar to the input.
+  if so it returns true and if not it returns false.
 */
 
 // You can run `node index.js` to view these console logs
